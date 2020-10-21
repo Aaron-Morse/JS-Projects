bd47a803cdacbfe845249b00bb4d3c83718e73d0
Aaron Morse <39176202+Aaron-Morse@users.noreply.github.com>
2020-10-21 13:12:30 -0700

    Add files via upload

--- Expense App/scripts/script.js
+++ Expense App/scripts/script.js
@@ -1,26 +1,39 @@
-const name = document.querySelector('input[id="name"]');
-const date = document.querySelector('input[id="date"]');
-const amount = document.querySelector('input[id="amount"]');
+// SELECTROS
+const nameInput = document.querySelector('input[id="name"]');
+const dateInput = document.querySelector('input[id="date"]');
+const amountInput = document.querySelector('input[id="amount"]');
 const table = document.querySelector('table.expense-table');
 const expenseButton = document.querySelector('button.add-expense');
 
-function tableRow() {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `<td>${name.value}</td><td>${date.value}</td><td>$${Number(amount.value).toFixed(2)}</td><td><button>x</button></td>`;
-    table.appendChild(tableRow);
-}
 
+// EVENT LISTENERS
+// Add item to expense table button listener
 expenseButton.addEventListener('click', () => {
-    tableRow();
-});
-
-table.addEventListener('click', event => {
-    event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
-});
-
-document.addEventListener('keydown', event => {
-    if (event.key === 'Enter' && name.value && date.value && amount.value) {
-        tableRow();
+    if (nameInput.value && dateInput.value && amountInput.value) {
+        addExpenseItem();
+    } else {
+        alert('Please fill out all expense item values.');
     }
-})
+});
 
+// Add item to expense table with the enter key listener
+table.addEventListener('click', event => {
+    if (event.target.nodeName === 'BUTTON') {
+        event.target.parentElement.parentElement.parentElement
+            .removeChild(event.target.parentElement.parentElement);
+    }
+});
+
+// Remove expense item from table button listener
+document.addEventListener('keydown', event => {
+    if (event.key === 'Enter' && nameInput.value && dateInput.value && amountInput.value) addExpenseItem();
+});
+
+//  FUNCTIONS
+function addExpenseItem() {
+    // Creates a new table row element and adds the table data with corresponding input values
+    const expenseItem = document.createElement('tr');
+    expenseItem.innerHTML = `<td>${nameInput.value}</td><td>${dateInput.value}</td><td>$${Number(amountInput.value).toFixed(2)}</td><td><button>x</button></td>`;
+    // Appends the newly created table row to the table
+    table.appendChild(expenseItem);
+}
